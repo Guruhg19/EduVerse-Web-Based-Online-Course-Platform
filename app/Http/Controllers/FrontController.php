399265a5957fc6369fdd7633f276fdf21219f50a
Guruhg19 <guruhvitra19@gmail.com>
2025-04-30 21:41:31 +0700

    Add FrontController with index and details methods; create index view and update routes

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function index()
+    {
+        return view('front.index');
+    }
+
+    public function details(Course $course)
+    {
+        return view('front.details', [
+            'course' => $course,
+        ]);
+    }
+
+}
