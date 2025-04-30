1f826b68560f8b211a77cad9ff9c782ed272306a
Guruhg19 <guruhvitra19@gmail.com>
2025-04-30 23:34:23 +0700

    Add form request classes for category, course, course video, and teacher management with validation rules and authorization checks

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
 use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
@@ -30,14 +31,12 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'icon' => 'required|image|mimes:png,jpg,jpeg|max:2048',
-        ]);
 
-        DB::transaction(function () use ($validated, $request){
+
+        DB::transaction(function () use ($request){
+            $validated = $request->validated();
             if($validated['icon']){
                 $iconPath = $request->file('icon')->store('icons', 'public');
                 $validated['icon'] = $iconPath;
