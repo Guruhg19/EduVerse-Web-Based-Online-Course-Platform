72c5d43283d6e5e8cadfae63174627fdfff72745
Guruhg19 <guruhvitra19@gmail.com>
2025-04-30 20:06:48 +0700

    Add SoftDeletes and fillable properties to model classes: Category, Course, CourseKeypoint, CourseStudent, SubscribeTransaction, and Teacher

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Course extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    
+    protected $fillable = [
+        'name',
+        'slug',
+        'path_trailer',
+        'about',
+        'thumbnail',
+        'category_id',
+        'teacher_id',
+    ];
 }
--- app/Models/SubscribeTransaction.php
+++ app/Models/SubscribeTransaction.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class SubscribeTransaction extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    
+    protected $fillable = [
+        'user_id',
+        'total_amount',
+        'is_paid',
+        'subscription_start_date',
+        'proof',
+    ];
 }
